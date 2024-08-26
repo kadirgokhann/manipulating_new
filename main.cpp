d8a38a420bab33ef0b762c53c9cf303363517152
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2024-08-26 10:53:34 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <new>
+
+class Object {
+public:
+    int data;
+    Object();
+};
+Object::Object()
+{
+    data = 10;
+}
+void explicit_constructor_call(Object* ptr)
+{
+    new (ptr) Object(); 
+}
+int main()
+{
+    label1:
+    void* memory = operator new(sizeof(Object));  // Allocate a memory block for the object.
+    Object* ptr  = static_cast<Object*>(memory);  // Cast the memory block to the object type.
+    //a->Object::Object();                        // Calling the constructor is not allowed by the compiler.
+    explicit_constructor_call(ptr);               // Call the constructor explicitly using placement new.                            
+    std::cout<< ptr->data;
+    return 0;
+}
